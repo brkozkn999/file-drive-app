8230ac163c8af1d4ffd53ec0f849ae1d85f3bc85
brkozkn999 <ibrahimberkozkan34@gmail.com>
2024-05-31 11:50:56 +0300

    initial setup of project

---
+++ convex/auth.config.ts
@@ -0,0 +1,8 @@
+export default {
+        providers: [
+            {
+                domain: "https://striking-monkey-92.clerk.accounts.dev",
+                applicationID: "convex",
+            },
+    ]
+};
